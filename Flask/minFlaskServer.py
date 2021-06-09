f277c1ea630a9eceabd0d9ed0064ee9e72fb1174
Abdul <abdulrehman.shoukat@gmail.com>
2021-06-09 16:19:22 -0700

    flask server boiler plate

--- Flask/minFlaskServer.py
+++ Flask/minFlaskServer.py
@@ -6,5 +6,11 @@ app = Flask(__name__)
 def hello_world():
     return "<p>Hello, World!</p>"
 
+
+@app.route("/user/<name>")
+def greet(name):
+    return f"Hello {name}"
+
+
 if __name__== '__main__':
-    app.run()
+    app.run(debug=True)
